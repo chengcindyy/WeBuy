b3beb36b91fba815b304701ca26fb736e989617f
melody_yu <m4745melody0706@gmail.com>
2023-05-28 11:45:06 -0700

    CustomerHomePage, Simplify TestActivity

---
+++ app/src/main/java/com/csis4495_cmk/webuy/fragments/CustomerHomePageFoodFragment.java
@@ -0,0 +1,28 @@
+package com.csis4495_cmk.webuy.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.csis4495_cmk.webuy.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CustomerHomePageFoodFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CustomerHomePageFoodFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer_home_page_new, container, false);
+    }
+}
