04690545201c6f879ccc354bad692ceaf00ceec4
melody_yu <m4745melody0706@gmail.com>
2023-06-15 15:03:04 -0700

    Merge branch 'melodysbranch' of /Users/melody_yu/Desktop/WeBuy with conflicts.

---
+++ app/src/main/java/com/csis4495_cmk/webuy/ItemMoveCallback.java
@@ -0,0 +1,77 @@
+package com.csis4495_cmk.webuy;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.csis4495_cmk.webuy.adapters.ImagesAdapter;
+
+public class ItemMoveCallback extends ItemTouchHelper.Callback{
+    private final ItemTouchHelperContract mAdapter;
+
+    public ItemMoveCallback(ItemTouchHelperContract adapter) {
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |ItemTouchHelper.UP | ItemTouchHelper.DOWN ;
+        return makeMovementFlags(dragFlags, 0);
+    }
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                          RecyclerView.ViewHolder target) {
+        mAdapter.onRowMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder,
+                                  int actionState) {
+
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof ImagesAdapter.AddImagesViewHolder) {
+                ImagesAdapter.AddImagesViewHolder myViewHolder=
+                        (ImagesAdapter.AddImagesViewHolder) viewHolder;
+                mAdapter.onRowSelected(myViewHolder);
+            }
+
+        }
+
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView,
+                          RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+
+        if (viewHolder instanceof ImagesAdapter.AddImagesViewHolder) {
+            ImagesAdapter.AddImagesViewHolder myViewHolder=
+                    (ImagesAdapter.AddImagesViewHolder) viewHolder;
+            mAdapter.onRowClear(myViewHolder);
+        }
+    }
+
+    public interface ItemTouchHelperContract {
+        void onRowMoved(int fromPosition, int toPosition);
+        void onRowSelected(ImagesAdapter.AddImagesViewHolder myViewHolder);
+        void onRowClear(ImagesAdapter.AddImagesViewHolder myViewHolder);
+    }
+
+}
