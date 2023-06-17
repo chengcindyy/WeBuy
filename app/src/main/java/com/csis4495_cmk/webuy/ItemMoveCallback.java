ed2fb705508ff25cef1a23bd5e158f6d0517ccb9
melody_yu <m4745melody0706@gmail.com>
2023-06-17 16:18:54 -0700

    1.seller add product and styles to realtime db 2.product image and style image to storage

--- app/src/main/java/com/csis4495_cmk/webuy/ItemMoveCallback.java
+++ app/src/main/java/com/csis4495_cmk/webuy/ItemMoveCallback.java
@@ -4,7 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.csis4495_cmk.webuy.adapters.ImagesAdapter;
+import com.csis4495_cmk.webuy.adapters.SellerAddProductImagesAdapter;
+import com.csis4495_cmk.webuy.adapters.SellerStyleListAdapter;
 
 public class ItemMoveCallback extends ItemTouchHelper.Callback{
     private final ItemTouchHelperContract mAdapter;
@@ -30,7 +31,12 @@ public class ItemMoveCallback extends ItemTouchHelper.Callback{
 
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-        int dragFlags = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |ItemTouchHelper.UP | ItemTouchHelper.DOWN ;
+        int dragFlags;
+        if (mAdapter instanceof SellerAddProductImagesAdapter) {
+            dragFlags = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        } else { //mAdapter instanceof SellerStyleListAdapter
+            dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        }
         return makeMovementFlags(dragFlags, 0);
     }
     @Override
@@ -45,9 +51,13 @@ public class ItemMoveCallback extends ItemTouchHelper.Callback{
                                   int actionState) {
 
         if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
-            if (viewHolder instanceof ImagesAdapter.AddImagesViewHolder) {
-                ImagesAdapter.AddImagesViewHolder myViewHolder=
-                        (ImagesAdapter.AddImagesViewHolder) viewHolder;
+            if (viewHolder instanceof SellerAddProductImagesAdapter.AddImagesViewHolder) {
+                SellerAddProductImagesAdapter.AddImagesViewHolder myViewHolder =
+                        (SellerAddProductImagesAdapter.AddImagesViewHolder) viewHolder;
+                mAdapter.onRowSelected(myViewHolder);
+            } else if (viewHolder instanceof SellerStyleListAdapter.StyleListViewHolder) {
+                SellerStyleListAdapter.StyleListViewHolder myViewHolder =
+                        (SellerStyleListAdapter.StyleListViewHolder)  viewHolder;
                 mAdapter.onRowSelected(myViewHolder);
             }
 
@@ -61,17 +71,19 @@ public class ItemMoveCallback extends ItemTouchHelper.Callback{
                           RecyclerView.ViewHolder viewHolder) {
         super.clearView(recyclerView, viewHolder);
 
-        if (viewHolder instanceof ImagesAdapter.AddImagesViewHolder) {
-            ImagesAdapter.AddImagesViewHolder myViewHolder=
-                    (ImagesAdapter.AddImagesViewHolder) viewHolder;
+        if (viewHolder instanceof SellerAddProductImagesAdapter.AddImagesViewHolder) {
+            SellerAddProductImagesAdapter.AddImagesViewHolder myViewHolder=
+                    (SellerAddProductImagesAdapter.AddImagesViewHolder) viewHolder;
             mAdapter.onRowClear(myViewHolder);
         }
     }
 
-    public interface ItemTouchHelperContract {
+    public interface ItemTouchHelperContract<T> {
         void onRowMoved(int fromPosition, int toPosition);
-        void onRowSelected(ImagesAdapter.AddImagesViewHolder myViewHolder);
-        void onRowClear(ImagesAdapter.AddImagesViewHolder myViewHolder);
+        //void onRowSelected(SellerAddProductImagesAdapter.AddImagesViewHolder myViewHolder);
+        void onRowSelected(T myViewHolder);
+        //void onRowClear(SellerAddProductImagesAdapter.AddImagesViewHolder myViewHolder);
+        void onRowClear(T myViewHolder);
     }
 
 }
